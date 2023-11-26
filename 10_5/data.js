4700f86f3b1d380f86ac249946234f9c108c2f56
dilia00 <dina_xenia@mail.ru>
2023-11-26 13:15:21 +0300

    HW_10(5)

---
+++ 10_5/data.js
@@ -0,0 +1,139 @@
+const dataInfo = `
+[
+    {
+        "id": 10261789,
+        "title": "Khan Younis",
+        "userName": "dhzuehlke",
+        "numViews": 1,
+        "numVotes": 0,
+        "numComments": 0,
+        "numHearts": 0,
+        "rank": 0,
+        "dateCreated": "2023-11-26 01:10:49",
+        "hex": "5F84D5",
+        "rgb": {
+            "red": 95,
+            "green": 132,
+            "blue": 213
+        },
+        "hsv": {
+            "hue": 221,
+            "saturation": 55,
+            "value": 84
+        },
+        "description": "",
+        "url": "http:\/\/www.colourlovers.com\/color\/5F84D5\/Khan_Younis",
+        "imageUrl": "http:\/\/www.colourlovers.com\/img\/5F84D5\/100\/100\/Khan_Younis.png",
+        "badgeUrl": "http:\/\/www.colourlovers.com\/images\/badges\/c\/10261\/10261789_Khan_Younis.png",
+        "apiUrl": "http:\/\/www.colourlovers.com\/api\/color\/5F84D5"
+    },
+    {
+        "id": 10261788,
+        "title": "Easter Egg",
+        "userName": "Skyblue1u",
+        "numViews": 2,
+        "numVotes": 0,
+        "numComments": 0,
+        "numHearts": 0,
+        "rank": 0,
+        "dateCreated": "2023-11-26 00:38:17",
+        "hex": "C37EFD",
+        "rgb": {
+            "red": 195,
+            "green": 126,
+            "blue": 253
+        },
+        "hsv": {
+            "hue": 273,
+            "saturation": 50,
+            "value": 99
+        },
+        "description": "",
+        "url": "http:\/\/www.colourlovers.com\/color\/C37EFD\/Easter_Egg",
+        "imageUrl": "http:\/\/www.colourlovers.com\/img\/C37EFD\/100\/100\/Easter_Egg.png",
+        "badgeUrl": "http:\/\/www.colourlovers.com\/images\/badges\/c\/10261\/10261788_Easter_Egg.png",
+        "apiUrl": "http:\/\/www.colourlovers.com\/api\/color\/C37EFD"
+    },
+    {
+        "id": 10261787,
+        "title": "Blue Sigh",
+        "userName": "Skyblue2u",
+        "numViews": 2,
+        "numVotes": 0,
+        "numComments": 0,
+        "numHearts": 0,
+        "rank": 0,
+        "dateCreated": "2023-11-26 00:34:31",
+        "hex": "7A9CD4",
+        "rgb": {
+            "red": 122,
+            "green": 156,
+            "blue": 212
+        },
+        "hsv": {
+            "hue": 217,
+            "saturation": 42,
+            "value": 83
+        },
+        "description": "",
+        "url": "http:\/\/www.colourlovers.com\/color\/7A9CD4\/Blue_Sigh",
+        "imageUrl": "http:\/\/www.colourlovers.com\/img\/7A9CD4\/100\/100\/Blue_Sigh.png",
+        "badgeUrl": "http:\/\/www.colourlovers.com\/images\/badges\/c\/10261\/10261787_Blue_Sigh.png",
+        "apiUrl": "http:\/\/www.colourlovers.com\/api\/color\/7A9CD4"
+    },
+    {
+        "id": 10261786,
+        "title": "Being Blue",
+        "userName": "Skyblue3u",
+        "numViews": 7,
+        "numVotes": 0,
+        "numComments": 0,
+        "numHearts": 0,
+        "rank": 0,
+        "dateCreated": "2023-11-26 00:06:46",
+        "hex": "3E649B",
+        "rgb": {
+            "red": 62,
+            "green": 100,
+            "blue": 155
+        },
+        "hsv": {
+            "hue": 215,
+            "saturation": 60,
+            "value": 61
+        },
+        "description": "",
+        "url": "http:\/\/www.colourlovers.com\/color\/3E649B\/Being_Blue",
+        "imageUrl": "http:\/\/www.colourlovers.com\/img\/3E649B\/100\/100\/Being_Blue.png",
+        "badgeUrl": "http:\/\/www.colourlovers.com\/images\/badges\/c\/10261\/10261786_Being_Blue.png",
+        "apiUrl": "http:\/\/www.colourlovers.com\/api\/color\/3E649B"
+    },
+    {
+        "id": 10261785,
+        "title": "Clear Water",
+        "userName": "Skyblue4u",
+        "numViews": 3,
+        "numVotes": 0,
+        "numComments": 0,
+        "numHearts": 0,
+        "rank": 0,
+        "dateCreated": "2023-11-25 23:50:46",
+        "hex": "7DEFED",
+        "rgb": {
+            "red": 125,
+            "green": 239,
+            "blue": 237
+        },
+        "hsv": {
+            "hue": 179,
+            "saturation": 48,
+            "value": 94
+        },
+        "description": "",
+        "url": "http:\/\/www.colourlovers.com\/color\/7DEFED\/Clear_Water",
+        "imageUrl": "http:\/\/www.colourlovers.com\/img\/7DEFED\/100\/100\/Clear_Water.png",
+        "badgeUrl": "http:\/\/www.colourlovers.com\/images\/badges\/c\/10261\/10261785_Clear_Water.png",
+        "apiUrl": "http:\/\/www.colourlovers.com\/api\/color\/7DEFED"
+    }
+]
+`
