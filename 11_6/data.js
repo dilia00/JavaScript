69f963281797a9c4750dca313f758660e8564cac
dilia00 <dina_xenia@mail.ru>
2023-12-02 19:47:00 +0300

    HW_11(6)

---
+++ 11_6/data.js
@@ -0,0 +1,40 @@
+const dataProducts = `
+[
+    {
+        "title": "MANGO  PEOPLE  T-SHIRT",
+        "price": 300,
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "image": "img/product_1.jpg"
+    },
+    {
+        "title": "ELLERY X M'O CAPSULE",
+        "price": 52,
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "image": "img/product_2.jpg"
+    },
+    {
+        "title": "MANGO  PEOPLE  T-SHIRT",
+        "price": 300,
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "image": "img/product_3.jpg"
+    },
+    {
+        "title": "ELLERY X M'O CAPSULE",
+        "price": 52,
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "image": "img/product_4.jpg"
+    },
+    {
+        "title": "MANGO  PEOPLE  T-SHIRT",
+        "price": 300,
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "image": "img/product_5.jpg"
+    },
+    {
+        "title": "ELLERY X M'O CAPSULE",
+        "price": 52,
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "image": "img/product_6.jpg"
+    }
+]
+`
